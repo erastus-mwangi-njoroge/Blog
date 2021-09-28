e15f1fa04371b2d10d19d4b34234f59861fba890
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-09-28 06:08:19 +0300

    add the main app to run the file

---
+++ manage.py
@@ -0,0 +1,29 @@
+from app import create_app,db
+from flask_script import Manager,Server
+from flask_migrate import Migrate,MigrateCommand
+from app.models import User,Article
+
+
+app = create_app('production')
+# app = create_app('testing')
+
+manager = Manager(app)
+migrate = Migrate(app,db)
+
+manager.add_command('db',MigrateCommand)
+manager.add_command('server',Server)
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover("tests")
+    unittest.TextTestRunner(verbosity=5).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return {'db' : db,'User':User , 'Article':Article}
+
+
+if __name__ == "__main__":
+    manager.run()
+    
