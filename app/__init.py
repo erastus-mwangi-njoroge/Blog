e692caa1199b408c487165ee62c7b39c49567945
Erastus Njoroge <erastus.njoroge@student.moringaschool.com>
2021-09-28 05:32:44 +0300

    initialise app

---
+++ app/__init.py
@@ -0,0 +1,37 @@
+from flask import Flask
+from config import configurations
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_uploads import configure_uploads,IMAGES,UploadSet
+
+app = Flask(__name__)
+db = SQLAlchemy()
+bootstrap = Bootstrap()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
+
+def create_app(configname):
+    '''
+    Method for creating the app to enable for easier configurations
+    '''
+    app.config.from_object(configurations[configname])
+
+    #Importing BluePrints
+    from .auth import auth as auth_blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(auth_blueprint)
+    app.register_blueprint(main_blueprint)
+
+    #Initialising the Importations
+    db.init_app(app)
+    bootstrap.init_app(app)
+    login_manager.init_app(app)
+
+    # configure UploadSet
+    configure_uploads(app,photos)
+
+    return app
+
